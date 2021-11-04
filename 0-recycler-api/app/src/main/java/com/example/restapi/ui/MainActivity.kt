5bc90b320e76a7fcc77e046ec4c53682c5cf358c
Noraluk <noraluk.kn@gmail.com>
2021-11-05 00:37:59 +0700

    :sparkles: add recycler

--- 0-recycler-api/app/src/main/java/com/example/restapi/ui/MainActivity.kt
+++ 0-recycler-api/app/src/main/java/com/example/restapi/ui/MainActivity.kt
@@ -6,14 +6,18 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.restapi.R
 import com.example.restapi.data.model.album.Album
 import com.example.restapi.databinding.ActivityMainBinding
+import com.example.restapi.ui.adapter.album.AlbumAdapter
 import com.example.restapi.viewmodel.album.AlbumViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private lateinit var albumViewModel: AlbumViewModel
+    private lateinit var albumAdapter: AlbumAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,51 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         albumViewModel = ViewModelProvider(this)[AlbumViewModel::class.java]
 
+        initAdapter()
+
         getAlbums()
     }
-
-//    private fun getAlbumsByUserId(userId:Int) {
-//        val hashMap = hashMapOf<String,String>()
-//        hashMap["userId"] = "1"
-//        hashMap["id"] = "2"
-//
-//        val call = apiService.getAlbumsByUserId(hashMap)
-//
-//        call.enqueue(object: Callback<List<Album>>{
-//            override fun onResponse(call: Call<List<Album>>, response: Response<List<Album>>) {
-//                if(response.isSuccessful) {
-//                    val list = response.body()
-//                    Log.i("API","${list!!.size}")
-//                    for (i in list!!.indices) {
-//                        val msg = "${list[i].id} \n userId: ${list[i].userId} \n title: ${list[i].album_title} \n"
-//                        binding.textView.append(msg)
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<List<Album>>, t: Throwable) {
-//                Log.e("API", t.message.toString())
-//            }
-//        })
-//    }
-
-//    private fun getAlbum(id: Int) {
-//        val call = apiService.getAlbums(id)
-//
-//        call.enqueue(object: Callback<Album>{
-//            override fun onResponse(call: Call<Album>, response: Response<Album>) {
-//                if(response.isSuccessful) {
-//                    val album = response.body()
-//                   Log.i("API",album.toString())
-//                    binding.textView.append(album.toString())
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<Album>, t: Throwable) {
-//                Log.e("API", t.message.toString())
-//            }
-//        })
-//    }
 
     private fun getAlbums() {
         albumViewModel.getAlbums()
@@ -74,13 +37,16 @@ class MainActivity : AppCompatActivity() {
         albumViewModel.getAlbumsLiveData?.observe(this, Observer {
             if (it!=null) {
                 var list = it as ArrayList<Album>
-                for (i in list!!.indices) {
-                        val msg = "${list[i].id} \n userId: ${list[i].userId} \n title: ${list[i].album_title} \n"
-                        binding.textView.append(msg)
-                }
+                albumAdapter.setData(list)
             }else{
                 Log.e("API","wrong")
             }
         })
+    }
+
+    private fun initAdapter() {
+        albumAdapter = AlbumAdapter()
+        binding.rvAlbum.layoutManager = LinearLayoutManager(this)
+        binding.rvAlbum.adapter = albumAdapter
     }
 }
